742b6c9bb1c6cba4f8e92d43cc45835b9c49a54d
Yi Sang the Ideal <avfama@student.apc.edu.ph>
2026-02-10 16:03:15 +0000

    Version 1.1 2/10/26

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the LCD Department!</h1>
+      <h4>The department for all Distortion and Abnormality related operations.</h4>
+    </div>
+  );
+}
+
+export default App;
